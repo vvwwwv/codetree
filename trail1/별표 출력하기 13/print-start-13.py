192ec3c6517cbb99b6475ccc71098edd15f74524
vvwwwv <105057394+vvwwwv@users.noreply.github.com>
2026-08-19 18:07:14 +0900

    [Passed] 별표 출력하기 13 (51ms, 16MB)

---
+++ trail1/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,12 @@
+n = int(input())
+a, b = 1, n
+for i in range(n*2):
+    if i % 2 == 0:
+        for j in range(b):
+            print("*", end=" ")
+        b -= 1
+    else:
+        for j in range(a):
+            print("*", end=" ")
+        a += 1
+    print()
