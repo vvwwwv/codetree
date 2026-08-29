d857995dc7c294502cddde7eedf3cc46e31968bc
vvwwwv <105057394+vvwwwv@users.noreply.github.com>
2026-08-30 01:18:28 +0900

    [Passed] 배열의 특정 위치 찾기 2 (48ms, 16MB)

--- trail1/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
+++ trail1/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -1,8 +1,6 @@
 arr = list(map(int, input().split()))
-sum1 = arr[::2]
-sum2 = arr[1::2]
-sum1 = sum(sum1)
-sum2 = sum(sum2)
+sum1 = sum(arr[::2])
+sum2 = sum(arr[1::2])
 if sum1 > sum2:
     print(sum1-sum2)
 else:
